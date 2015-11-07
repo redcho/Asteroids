cbe12f35e9478c0d584aa7570b462bdd93b5ccda
redcho <manis.eren@gmail.com>
2015-11-08 01:18:14 +0200

    Initial commit

---
+++ desktop/src/com/redcho/asteroid/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.redcho.asteroid.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.redcho.asteroid.ZMainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZMainGame(), config);
+	}
+}
